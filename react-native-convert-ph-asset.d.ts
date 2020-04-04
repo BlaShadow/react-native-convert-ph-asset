d1a0eff938cd2b7c6fa8cc897b52fc10a2a3fcc4
blashadow <blackzerogamer@gmail.com>
2020-04-04 11:44:30 -0400

    Add library types

---
+++ react-native-convert-ph-asset.d.ts
@@ -0,0 +1,27 @@
+declare module "react-native-convert-ph-asset" {
+  import { EmitterSubscription } from "react-native";
+
+  export type ConvertPHAssetEvent = 'VideoProcessingProgress' | 'DownloadAssetProgress';
+
+  export interface ConvertAssetEventResponse {
+    progress: number;
+    assetId: string;
+  }
+
+  export type ConvertAssetPHEventResponse = (info: ConvertAssetEventResponse) => void
+
+  export type ConvertAssetExportFormat = 'mpeg4' | 'm4v' | 'mov';
+
+  export type ConvertAssetExportQuality = 'high' | 'medium' | 'low' | 'original' | 'aVAssetExportPreset640x480' | 'aVAssetExportPreset960x540' | 'aVAssetExportPreset1280x720';
+
+  export interface ConvertPHAssetParams {
+    url: string,
+    convertTo: ConvertAssetExportFormat,
+    quality: ConvertAssetExportQuality
+  }
+
+  export default interface ConvertPHAsset {
+    convertVideoFromUrl: (params: ConvertPHAssetParams) => Promise<string>
+    registerToEvent: (eventName: ConvertPHAssetEvent, handler: ConvertAssetPHEventResponse) => EmitterSubscription
+  }
+}
